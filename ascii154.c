5d7f9a64bd784ae1d59feae3e3f835eb1567070d
rekha-nagarajan <95352086+rekha-nagarajan@users.noreply.github.com>
2021-12-11 11:21:51 +0530

    Update ascii154.c

--- ascii154.c
+++ ascii154.c
@@ -1,30 +1,12 @@
 #include<stdio.h>
 #include<conio.h>
-char const* character[]={"NULL","SOH","STX", "ETX", "EOT", "ENQ", "ACK",
-                         "BEL","BS","TAB","LF","VT","FF","CR","SO", "SI",
-                         "DLE", "DC1", "DC2", "DC3", "DC4", "NAK", "SYN",
-                         "ETB","CAN", "EM", "SUB", "ESC", "FS", "GS", "RS", "US","SPA"};
 int main()
 {
-    char c;
-    int row;
-    printf("ASCII CHART\n==========================\n");
-    for(int i=0; i<33; i++)
-        {
-        row=i;
-        while(row<=127){
-            if(row<33)
-                printf("\t%d=%s\t",row,character[i]);
-
-            else if(row>=33 && row<127){
-                    c=row;
-                    printf("\t%d=%c\t",row,c);
-            }
-
-            else
-                printf("\t%d=DEL\t",row);
-            row=row+33;
-        }
-        printf("\n");
-}
+   unsigned char i;
+   printf("ASCII CHART\n==========================\n");
+   for( i=0; i<127; i++)
+       {
+        printf("\n%d\t=\t%c\n",i,i);
+   }
+  return 0;
 }
